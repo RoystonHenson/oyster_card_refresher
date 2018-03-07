7ce7e149a1a931c19d2e9b89ada25b50f7f937ad
Royston Henson <rlqhenson@hotmail.com>
2018-03-07 18:21:57 +0000

    Add feature: card can be topped up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,11 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe '#top_up' do
+    it 'tops up card' do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+  end
 end
