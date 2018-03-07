3b812f195d4d54e019b54225e5169c657e8c22a9
Royston Henson <rlqhenson@hotmail.com>
2018-03-07 18:47:50 +0000

    Replace magic numbers

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,11 @@
 class Oystercard
   attr_reader :balance
 
+  START_BALANCE = 10
   MAX_BALANCE = 90
 
   def initialize
-    @balance = 0
+    @balance = START_BALANCE
   end
 
   def top_up(amount)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,15 +3,16 @@ require 'oystercard'
 describe Oystercard do
   describe '#initialize' do
     it 'initialises with a balance' do
-      subject { Oystercard.new(0)   }
-      expect(subject.balance).to eq(0)
+      subject { Oystercard.new(Oystercard::START_BALANCE) }
+      p subject.balance
+      expect(subject.balance).to eq(Oystercard::START_BALANCE)
     end
   end
 
   describe '#top_up' do
     it 'tops up card' do
       subject.top_up(5)
-      expect(subject.balance).to eq(5)
+      expect(subject.balance).to eq(Oystercard::START_BALANCE + 5)
     end
 
     it 'raises error if over maximum balance' do
