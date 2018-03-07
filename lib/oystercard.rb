050e6d9f83938b4b79a089c1b64e39377c996824
Royston Henson <rlqhenson@hotmail.com>
2018-03-07 19:06:33 +0000

    Refactor tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,7 @@
 class Oystercard
   attr_reader :balance
 
-  START_BALANCE = 10
+  START_BALANCE = 5
   MAX_BALANCE = 90
 
   def initialize
@@ -11,5 +11,9 @@ class Oystercard
   def top_up(amount)
     raise "Maximum balance allowed is £#{MAX_BALANCE}." if balance + amount > MAX_BALANCE
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,20 +3,23 @@ require 'oystercard'
 describe Oystercard do
   describe '#initialize' do
     it 'initialises with a balance' do
-      subject { Oystercard.new(Oystercard::START_BALANCE) }
-      p subject.balance
       expect(subject.balance).to eq(Oystercard::START_BALANCE)
     end
   end
 
   describe '#top_up' do
     it 'tops up card' do
-      subject.top_up(5)
-      expect(subject.balance).to eq(Oystercard::START_BALANCE + 5)
+      expect { subject.top_up(5) }.to change { subject.balance }.by(5)
     end
 
     it 'raises error if over maximum balance' do
       expect { subject.top_up(91) }.to raise_error("Maximum balance allowed is £#{Oystercard::MAX_BALANCE}.")
+    end
+  end
+
+  describe '#deduct' do
+    it 'deducts fare from balance' do
+      expect { subject.deduct(1) }.to change { subject.balance }.by(-1)
     end
   end
 end
